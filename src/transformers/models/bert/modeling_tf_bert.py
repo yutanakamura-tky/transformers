a87bfaf2f9993af8362e870bc7d43efbeb2c19b0
ydshieh <ydshieh@users.noreply.github.com>
2022-05-06 19:57:06 +0200

    test push ci

--- src/transformers/models/bert/modeling_tf_bert.py
+++ src/transformers/models/bert/modeling_tf_bert.py
@@ -759,7 +759,7 @@ class TFBertMainLayer(tf.keras.layers.Layer):
     ) -> Union[TFBaseModelOutputWithPoolingAndCrossAttentions, Tuple[tf.Tensor]]:
 
         if not self.config.is_decoder:
-            use_cache = False
+            use_cache = True
 
         if input_ids is not None and inputs_embeds is not None:
             raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
